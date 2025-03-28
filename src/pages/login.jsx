95109268b44416a531f9046dec83f110db68fe2b
carolinasntos <a01174992@tec.mx>
2025-03-27 20:06:51 -0600

    :D

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+//import * as React from 'react';
 import { AppProvider } from '@toolpad/core/AppProvider';
 import { SignInPage } from '@toolpad/core/SignInPage';
 import { useTheme } from '@mui/material/styles';
--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+//import React, { useState } from "react";
 import { TextField, Button, Typography, Alert } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 
