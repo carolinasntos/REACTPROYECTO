ff553ac4aa44bce587a7dfcf01ec44849f7daa44
carolinasntos <a01174992@tec.mx>
2025-04-01 17:53:29 -0600

    D

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+/*import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'*/
 
-// https://vite.dev/config/
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/mi-proyecto-react/', // <-- el nombre exacto de tu repositorio
   plugins: [react()],
-  base: '/ReactProyecto/',
-});
+})
