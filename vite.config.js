3076754edb3e3dff6e761ffc96aaba03b93cb4df
carolinasntos <a01174992@tec.mx>
2025-04-01 18:30:10 -0600

    D

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/mi-proyecto-react/', // <-- el nombre exacto de tu repositorio
+  base: '/ReactProyecto/', // <-- el nombre exacto de tu repositorio
   plugins: [react()],
 })
